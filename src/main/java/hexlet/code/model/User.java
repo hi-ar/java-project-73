04b5be8906a917aada915f7aeacaafeee96e140b
Artem (air) <artra@ya.ru>
2023-08-22 00:08:29 +0300

    821-1 2step

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -10,11 +10,16 @@ package hexlet.code.model;
  */
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
+import java.util.Date;
 
 @Entity
 @Data
@@ -30,5 +35,5 @@ public class User {
     private String password;
 
     @CreationTimestamp
-    private Instant createdAt;
+    private Date createdAt;
 }
